abc8092c8efea9ebc5326cd75e8711082803c37a
Onyx0-1 <damiangg199@gmail.com>
2025-06-28 01:15:12 -0400

    cambio1

--- Modelo 1/Ejercicios de Python/Ejercicios/Ejercicio1.py
+++ Modelo 1/Ejercicios de Python/Ejercicios/Ejercicio1.py
@@ -3,5 +3,5 @@ edad = int(input("Por favor, dime tu edad: "))
 if edad >= 18:
     print("Eres mayor de edad.")
 else:
-    print("No eres mayor de edad..")
+    print("No eres mayor de edad.")
     
