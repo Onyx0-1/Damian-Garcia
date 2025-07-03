2c70176def56d5c5bea3be1f367126513807074a
Onyx0-1 <damiangg199@gmail.com>
2025-07-03 13:32:03 -0400

    olololo

--- main.js
+++ main.js
@@ -4,19 +4,15 @@ const scrollRevealOption = {
   duration: 1000,
 };
 
-ScrollReveal().reveal(".header__image img", {
-  ...scrollRevealOption,
+ScrollReveal().reveal(".header__image img", {scrollRevealOption,
   origin: "right",
 });
-ScrollReveal().reveal(".header__content h1", {
-  ...scrollRevealOption,
+ScrollReveal().reveal(".header__content h1", {scrollRevealOption,
   delay: 500,
 });
-ScrollReveal().reveal(".header__content p", {
-  ...scrollRevealOption,
+ScrollReveal().reveal(".header__content p", {scrollRevealOption,
   delay: 1000,
 });
-ScrollReveal().reveal(".header__content form", {
-  ...scrollRevealOption,
+ScrollReveal().reveal(".header__content form", {scrollRevealOption,
   delay: 1500,
 });
